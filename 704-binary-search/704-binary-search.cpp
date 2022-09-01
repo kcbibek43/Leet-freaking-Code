ca981ac11ccdfcace99dd5f089914e7d2c06e237
kcbibek43 <101321124+kcbibek43@users.noreply.github.com>
2022-09-01 18:48:06 +0530

    Time: 124 ms (5.47%), Space: 27.4 MB (99.70%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-    int binary(vector<int> nums,int k,int low,int high){
-        if(low>high){
-            return -1;
-        }
-        int ans = 0;
-        int mid=(low+high)/2;
-            if(nums[mid]==k){
-              return mid;
-            }
-            else if(nums[mid]<k){
-               return binary(nums,k,mid+1,high);
-            }
-               return binary(nums,k,low,mid-1);
-    }
+    // int binary(vector<int> nums,int k,int low,int high){
+    //     if(low>high){
+    //         return -1;
+    //     }
+    //     int ans = 0;
+    //     int mid=(low+high)/2;
+    //         if(nums[mid]==k){
+    //           return mid;
+    //         }
+    //         else if(nums[mid]<k){
+    //            return binary(nums,k,mid+1,high);
+    //         }
+    //            return binary(nums,k,low,mid-1);
+    // }
     int search(vector<int>& nums, int target) {
        int n = nums.size()-1;
         int l = 0;
         int h = n;
         int mid =0;
-        // while(l<=h){
-        //     mid=(l+h)/2;
-        //     if(nums[mid]==target){
-        //         return mid;
-        //     }
-        //     else if(nums[mid]<target){
-        //         l = mid+1;
-        //     }
-        //     else{
-        //         h = mid-1;
-        //     }
-        // }
-        
-        return binary(nums,target,0,nums.size()-1);
+        while(l<=h){
+            mid=(l+h)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                l = mid+1;
+            }
+            else{
+                h = mid-1;
+            }
+        }
+     
+        return -1;
+        // return binary(nums,target,0,nums.size()-1);
     }
 };
