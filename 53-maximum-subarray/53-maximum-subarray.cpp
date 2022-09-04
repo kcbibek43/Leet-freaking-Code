2cdda94c31c892fa1c6b319eacdae911aafde682
kcbibek43 <101321124+kcbibek43@users.noreply.github.com>
2022-09-04 13:39:00 +0530

    Time: 136 ms (79.09%), Space: 70.5 MB (6.15%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-     //    vector<int> dp(nums.size(),0);
-     //    if(nums.size()==1){
-     //        return nums[0];
-     //    }
-     //     dp[0] = nums[0];
-     //    for(int i = 1 ; i < nums.size() ; i++ ){
-     //    int sum = nums[i] + dp[i-1];
-     //        dp[i]=max(sum,nums[i]);
-     //    }
-     //    int mx=0;
-     // for(int i = 0;i<dp.size();i++){
-     //     mx=max(dp[i],mx);
-     // }
-        int sum=0,mx=INT_MIN;
-        for(int i:nums){
-            sum+=i;
-            mx=max(mx,sum);
-            if(sum<0)
-                sum=0;
+        vector<int> dp(nums.size(),0);
+        if(nums.size()==1){
+            return nums[0];
         }
+         dp[0] = nums[0];
+        for(int i = 1 ; i < nums.size() ; i++ ){
+        int sum = nums[i] + dp[i-1];
+            dp[i]=max(sum,nums[i]);
+        }
+        int mx=INT_MIN;
+     for(int i = 0;i<dp.size();i++){
+         mx=max(dp[i],mx);
+     }
+        // int sum=0,mx=INT_MIN;
+        // for(int i:nums){
+        //     sum+=i;
+        //     mx=max(mx,sum);
+        //     if(sum<0)
+        //         sum=0;
+        // }
         return mx;
     }
 };
