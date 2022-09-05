fd517ecf5651db746d4061f6feb77e1042fc660c
kcbibek43 <101321124+kcbibek43@users.noreply.github.com>
2022-09-06 01:08:59 +0530

    Time: 617 ms (5.02%), Space: 85.7 MB (11.39%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -4,7 +4,7 @@ public:
     //  int prev=0;
     //     int xOr=nums[0];
     //     int ans=0;
-    //     int n=nums.size();
+         int n=nums.size();
     //     for(int i=1;i<n;i++){
     //      // xOr=xOr^nums[i];
     //     if((prev^nums[i])==xOr || (nums[i]^nums[i-1])==0){
@@ -15,11 +15,20 @@ public:
     // prev = xOr;
     //     }
     //     return ans;
-         unordered_set<int> seen;
-        for (auto &num : nums) {
-            if (seen.count(num))
-                return num;
-            seen.insert(num);
+        //  unordered_set<int> seen;
+        // for (auto &num : nums) {
+        //     if (seen.count(num))
+        //         return num;
+        //     seen.insert(num);
+        // }
+        // return -1;
+        set<int> st;
+        st.insert(nums[0]);
+        for(int i=1;i<n;i++){
+            if(st.find(nums[i])!=st.end()){
+        return nums[i];
+            }
+            st.insert(nums[i]);
         }
         return -1;
     }
